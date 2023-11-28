f257ea7555daa32401df8bdb3e431e1b66df6eb0
RuRi-Code-Cloud <rudziter367@gmail.com>
2023-11-28 01:04:17 +0000

    deleted unnecessary code

--- src/script-bedroom.js
+++
@@ -1,8 +0,0 @@
-function myFunction(el) {
-  var x = document.getElementById(el);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
